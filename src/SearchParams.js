d743d293742e106c73ce781ab5ab7290a78214c0
Alfonso Rognoni <alfonso@7lfreight.com>
2019-10-07 16:10:07 -0300

    delete old dropdowns

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -23,40 +23,6 @@ const SearchParams = () => {
             onChange={event => setLocation(event.target.value)}
           />
         </label>
-        {/* <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={event => setAnimal(event.target.value)}
-            onBlur={event => setAnimal(event.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map(animalString => (
-              <option key={animalString} value={animalString}>
-                {animalString}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={event => setBreed(event.target.value)}
-            onBlur={event => setBreed(event.target.value)}
-            disabled={breeds.length === 0}
-          >
-            <option>All</option>
-            {breeds.map(breedString => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label> */}
         <AnimalDropdown />
         <BreedDropdown />
         <button>Submit</button>
