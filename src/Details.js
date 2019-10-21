e3873a550da398a7e5db77f6699422ff5993aee5
Alfonso Rognoni <alfonso@7lfreight.com>
2019-10-21 15:49:51 -0300

    error boundary - react context

--- src/Details.js
+++ src/Details.js
@@ -1,15 +1,10 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       loading: true
-  //     };
-  //   }
-
   state = { loading: true };
 
   componentDidMount() {
@@ -38,7 +33,11 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
@@ -46,4 +45,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
