a67d47f3dd1e02a8d2831e4a4cdb17e698196bc3
Alfonso 7l <alfonso@7lfreight.com>
2020-01-27 14:34:39 -0300

    playing with css (emotion-js)

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,38 @@
 import React, { useState } from 'react';
 import { Link } from '@reach/router';
-import { css } from '@emotion/core';
+import { css, keyframes } from '@emotion/core';
+import colors from './colors'
+
+const spin = keyframes`
+    to{
+        transform: rotate(360deg)
+    }
+`;
+
+const checkSpeed = (speed) => speed < .3 ? .3 : speed;
 
 const NavBar = () => {
-    const [padding, setpadding] = useState(15)
+    const [padding] = useState(15);
+    const [speed, setSpeed] = useState(1);
     return (
         <header
-            onClick={() => setpadding(padding + 15)}
             css={css`
-                background-color: pink;
+                background-color: ${colors.secondary};
                 padding: ${padding}px;
             `}
         >
             <Link to="/">Adopt Me!</Link>
-            <span css={css`font-size: 60px;`} role="img" aria-label="logo">🐩</span>
+            <span css={css`
+                font-size: 60px;
+                display: inline-block;
+                animation: ${spin} ${speed}s linear infinite;
+                &:hover {
+                    text-decoration: underline;
+                    animation: ${spin} 1s linear infinite reverse;
+                }
+            `}
+                onClick={() => setSpeed(checkSpeed(speed / 1.5))}
+                role="img" aria-label="logo">🐩</span>
         </header>
     )
 }
---
+++ src/colors.js
@@ -0,0 +1,6 @@
+export default {
+    primary: "pink",
+    secondary: "#d9c148",
+    dark: "black",
+    light: "yellow"
+}
